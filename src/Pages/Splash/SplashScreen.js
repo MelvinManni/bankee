ada9cd1ba5d2375827c89a1821fbbcb9a5ac5676
Melvin Kosisochukwu <melvinnnamani@gmail.com>
2020-12-14 05:04:55 +0100

    completed Verify number screen: navigate signup > with phone number > send code

--- src/Pages/Splash/SplashScreen.js
+++ src/Pages/Splash/SplashScreen.js
@@ -25,7 +25,7 @@ const SplashScreen = ({navigation}) => {
       }),
     ]).start(() => {
       setTimeout(() => {
-        navigation.replace('SignUp');
+        navigation.replace('VerifyNumber');
       }, 50);
     });
   }, []);
